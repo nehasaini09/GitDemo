b27049dbe4c98a56bba4ae28b9ff64812d7166fd
neha <nehasaini.mca09@gmail.com>
2023-06-23 16:54:30 -0700

    this is commit from Asia time zonee

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -7,6 +7,7 @@ public class AppiumTest {
 	@Test
 	public void NativeApppAndroid() {
 		System.out.println("NativeApppAndroid");
+		System.out.println("Git Added");
 	}
 	@Test
 	public void IOSApps() {
